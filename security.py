26fbf895bb2c04f8f0c20eef3c855ba517b3159b
aarunbala <aarunbala@gmail.com>
2020-08-10 22:48:46 +1000

    first commit

---
+++ security.py
@@ -0,0 +1,16 @@
+from werkzeug.security import safe_str_cmp
+from user import User
+
+users = [ User(1, 'bob', 'b0b')]
+
+username_mappings = {u.username: u for u in users}
+userid_mappings = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_mappings.get(username, None)
+    if user and safe_str_cmp(user.password, password):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_mappings.get(user_id, None)
---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    """class to maintainu User data"""
+
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
